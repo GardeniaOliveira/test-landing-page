2818121400eee7fda9d7b77d161354041797a246
Gardenia Oliveira <jobs.gardeniaoliveira@gmail.com>
2022-11-21 22:04:04 +0000

    uptade validate and uptade next page

--- main.js
+++ main.js
@@ -8,6 +8,8 @@ const radioFemale = document.getElementById('female')
 const error = document.querySelectorAll('.error');
 const success = document.querySelector('.success');
 let formComplete = true;
+let formError = false;
+let url = 'https://frontend-intern-challenge-api.iurykrieger.now.sh/products?page=1'
 let imageProduct = document.querySelector('.image');
 let nameProduct = document.querySelector('.product-name');
 let descriptionProduct = document.querySelector('.product-description');
@@ -29,86 +31,133 @@ btnSubscribe.addEventListener("click", (e) => {
     e.preventDefault();
     validateForm();
 
-    if(formComplete === true){
+    if (formComplete === true && formError === false) {
         messageSuccess();
         cleanForm();
     }
 
 })
 formSubscribe.addEventListener("focus", (e) => {
-        cleanForm();
+    cleanForm();
 
 })
+function validateName(name) {
+    if (name === "") {
+        formError = true;
+        error[0].innerText = "Por favor, digite seu nome.";
+        formSubscribe.name.focus;
 
-function validateForm() {
-   if(inputName.value ===""){
-    error[0].innerText = "Por favor, digite seu nome.";
-    formSubscribe.name.focus;
+        inputName.style.border = "1px solid red";
+        formComplete = false;
 
-    inputName.style.border = "1px solid red";
-    formComplete = false; 
-   }
-   else{
-    error[0].innerText = "";
-    inputName.style.border = "1px solid green";
-    formComplete = true;
-   }
+    }
+    else {
+        error[0].innerText = "";
+        inputName.style.border = "1px solid green";
+        formComplete = true;
+    }
 
-   if( inputEmail.value ===""){
-    error[1].innerText = "Por favor, digite seu e-mail.";
-    formSubscribe.email.focus;
+}
+function validateEmail(email) {
+    if (email === "") {
+        error[1].innerText = "Por favor, digite seu e-mail.";
+        formSubscribe.email.focus;
 
-    inputEmail.style.border = "1px solid red";
-    formComplete = false; 
-   }
-   else{
-    error[1].innerText = "";
-    inputEmail.style.border = "1px solid green";
-     formComplete = true;
-   }
+        inputEmail.style.border = "1px solid red";
+        formComplete = false;
+        formError = true;
+    }
+    else {
+        error[1].innerText = "";
+        inputEmail.style.border = "1px solid green";
+        formComplete = true;
+    }
 
-   if(inputCpf.value ===""){
-    error[2].innerText = "Por favor, digite seu CPF.";
-    formSubscribe.cpf.focus;
+    let validateRegexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    if (email.match(validateRegexEmail)) {
+        error[1].innerText = "";
+        inputEmail.style.border = "1px solid green";
+        formComplete = true;
+    }
+    else {
+        error[1].innerText = "Por favor, digite um e-mail válido.";
+        formSubscribe.email.focus;
 
-    inputCpf.style.border = "1px solid red";
-    formComplete = false; 
-   }
-   else{
-    error[2].innerText = "";
-    inputCpf.style.border = "1px solid green";
-    formComplete = true;
-   }
-   if(inputCpf.value.length !== 11){
-    error[2].innerText = "O CPF precisa conter 11 dígitos.";
-    formSubscribe.cpf.focus;
-console.log(formSubscribe.cpf.value.length)
-    inputCpf.style.border = "1px solid red";
-    formComplete = false; 
-   }
-   else{
-    error[2].innerText = "";
-  formComplete = true;
-   }
-    if(radioMale.checked === false && radioFemale.checked === false){
+        inputEmail.style.border = "1px solid red";
+        formComplete = false;
+        formError = true;
+
+    }
+}
+function validateCpf(cpf) {
+    if (cpf === "") {
+        formError = true;
+        error[2].innerText = "Por favor, digite seu CPF.";
+        formSubscribe.cpf.focus;
+
+        inputCpf.style.border = "1px solid red";
+        formComplete = false;
+        formError = true;
+    }
+    else {
+        error[2].innerText = "";
+        inputCpf.style.border = "1px solid green";
+        formComplete = true;
+    }
+    // if (cpf.length !== 11) {
+    //     error[2].innerText = "O CPF precisa conter 11 dígitos.";
+    //     formSubscribe.cpf.focus;
+    //     console.log(formSubscribe.cpf.value.length)
+    //     inputCpf.style.border = "1px solid red";
+    //     formComplete = false;
+    //     formError = true;
+    // }
+    // else {
+    //     error[2].innerText = "";
+    //     formComplete = true;
+    // }
+
+    let validateRegexCpf = /^\d{3}\.\d{3}\.\d{3}\-\d{2}$/
+    if (cpf.match(validateRegexCpf)) {
+        error[2].innerText = "";
+        formComplete = true;
+    }
+    else {
+        error[2].innerText = "Digite um CPF válido";
+        formSubscribe.cpf.focus;
+        inputCpf.style.border = "1px solid red";
+        formComplete = false;
+        formError = true;
+    }
+}
+function validateGender() {
+    if (radioMale.checked === false && radioFemale.checked === false) {
         error[3].innerText = "Escolha um sexo";
-        formComplete = false; 
-   }else{
-    error[3].innerText = "";
-    formComplete = true;
+        formComplete = false;
+        formError = true;
+    } else {
+        error[3].innerText = "";
+        formComplete = true;
 
 
-   }
+    }
 }
 
-function messageSuccess(){
+function validateForm() {
+    validateName(inputName.value)
+    validateEmail(inputEmail.value)
+    validateCpf(inputCpf.value)
+    validateGender()
+}
+
+function messageSuccess() {
     success.innerText = "Formulário enviado com sucesso!";
-   
+
 }
 function cleanForm() {
     inputName.value = "";
     inputEmail.value = "";
-    inputCpf.value = "" ;
+    inputCpf.value = "";
     radioMale.checked === false;
     radioFemale.checked === false;
     newsletterInputName.value = "";
@@ -116,23 +165,25 @@ function cleanForm() {
 }
 
 
-function showProducts(){
-    fetch(`https://frontend-intern-challenge-api.iurykrieger.now.sh/products?page=1`)
-    .then(response => {
-        response.json().then(data => {
-            putOnProductContainer(data)
-            console.log(data);
-
+function showProducts() {
+    fetch(url)
+        .then(response => {
+            response.json().then(data => {
+                putOnProductContainer(data)
+                console.log(data);
+                url = `https://${data.nextPage}`; //a url irá redirecionar para a próxima pagina| estava faltando o http no link da api
+            })
         })
-    })
-    .catch(error => console.error(error));
+        .catch(error => console.error(error));
+
+        
 }
-    
+
 showProducts()
 
-  function putOnProductContainer(data) {
+function putOnProductContainer(data) {
     for (let i = 0; i < data.products.length; i++) {
-        productsGrid.innerHTML = productsGrid.innerHTML+`
+        productsGrid.innerHTML = productsGrid.innerHTML + `
         <div class="product-container">
         <div class="product-img">
             <img src="${data.products[i].image}" alt="" class="image">
@@ -150,10 +201,11 @@ showProducts()
         
         `
     }
-  }
+}
 
-  btnNextPage.addEventListener("click", (e) => {
+btnNextPage.addEventListener("click", (e) => {
     e.preventDefault();
+   
     showProducts()
 
 })
@@ -161,43 +213,55 @@ showProducts()
 btnNewsletter.addEventListener("click", (e) => {
     e.preventDefault();
     validateFormNewsletter();
-    if(formComplete === true){
+    if (formComplete === true) {
         sendNewslleter();
         cleanForm();
     }
-    
-    
 
 })
-function validateFormNewsletter (){
-    if(newsletterInputName.value ===""){
+function validateFormNewsletter() {
+    if (newsletterInputName.value === "") {
         error[4].innerText = "Por favor, digite seu nome.";
         formNewsletter.name.focus;
-    
+
         newsletterInputName.style.border = "1px solid red";
-        formComplete = false; 
-       }
-       else{
+        formComplete = false;
+    }
+    else {
         error[4].innerText = "";
         newsletterInputName.style.border = "1px solid green";
-         formComplete = true;
-       }
-    
-       if( newsletterInputEmail.value ===""){
+        formComplete = true;
+    }
+
+    if (newsletterInputEmail.value === "") {
         error[5].innerText = "Por favor, digite seu e-mail.";
         formNewsletter.email.focus;
-    
+
         newsletterInputEmail.style.border = "1px solid red";
-        formComplete = false; 
-       }
-       else{
+        formComplete = false;
+    }
+    else {
         error[5].innerText = "";
         newsletterInputEmail.style.border = "1px solid green";
-         formComplete = true;
-       }
+        formComplete = true;
+    }
+    let validateRegexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    if (newsletterInputEmail.value.match(validateRegexEmail)) {
+        error[5].innerText = "";
+        newsletterInputEmail.style.border = "1px solid green";
+       
+    }
+    else {
+        error[5].innerText = "Por favor, digite um e-mail válido.";
+        formNewsletter.email.focus;
+
+        newsletterInputEmail.style.border = "1px solid red";
+        formComplete = false;
+
+    }
 
 }
-function sendNewslleter(){
-   sendNewslleterMessage.innerText = "Obrigada por assinar nossa newslleter. Verifique sua caixa de e-mail!";
-   
+function sendNewslleter() {
+    sendNewslleterMessage.innerText = "Obrigada por assinar nossa newslleter. Verifique sua caixa de e-mail!";
+
 }
